3b300618c13073317c0028988673fcb02a2c6104
chingvern <khcy6ocv@nottingham.edu.my>
2018-11-30 16:25:50 +0800

    added php for getting html source code

--- dropdown test.php
+++ dropdown test.php
@@ -29,5 +29,19 @@
   <input type="submit">
 </form>
 
+<?php
+    $curl_handle=curl_init();
+    curl_setopt($curl_handle, CURLOPT_URL,'https://dumps.wikimedia.org/backup-index.html');
+    curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
+    curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl_handle, CURLOPT_USERAGENT, 'Mozilla/5.0');
+    curl_setopt($curl_handle, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($curl_handle, CURLOPT_SSL_VERIFYHOST, false);
+    $query = curl_exec($curl_handle);
+    curl_close($curl_handle);
+    $query =  htmlentities($query);
+    echo $query;
+?>
+
 </body>
 </html>
