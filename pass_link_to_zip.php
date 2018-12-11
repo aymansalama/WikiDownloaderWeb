3e2a521684971f4edf5c6df7c22e1e0258c8eb41
hsienen <khcy6hhe@nottingham.edu.my>
2018-12-12 04:20:30 +0800

    integrate pass_link_to_zip.php with downloadzip.php

--- pass_link_to_zip.php
+++ pass_link_to_zip.php
@@ -1,39 +1,109 @@
 <!DOCTYPE html>
-
+<html>
 <body>
 <?php
-//$url = 'https://dumps.wikimedia.org/enwiki/latest/' ;
+
 $url = $_POST['urlBox2'];
-$stack = array();
+$sizeArray = array();
+$selectedArray = array();
+$fileArray = array();
+
 if(isset($_POST['submit'])){
 	if(!empty($_POST['match_values'])){
-		foreach($_POST['match_values']as $selected){
-			//echo ('<a href="'.$url.$selected.'">'.$selected.'</a><br>');
-			array_push($stack, $url.$selected);	
+		for($i = 0; $i < count($_POST['match_values']); $i++){
+			$selected = $_POST['match_values'][$i];
+			$URL = $url.$selected;
+			$file = get_download_file($URL, $selected, $sizeArray[$i]);
+			array_push($selectedArray, $selected);
+			array_push($fileArray, $file);
 		}
-		//$link = "'".implode("','",array_unique($stack))."'";
-		//$stack = array($link);
-		//$arr = explode(",",$link);
-		//print_r($stack);
-		//print_r($link);
-		//print_r($arr);
 	}
 }
 
-$zip = new ZipArchive();
-$tmp_file = tempnam('.','');
-$zip->open($tmp_file, ZipArchive::CREATE);
 
-foreach($stack as $file){
-	$download_file = file_get_contents($file);
-	$zip->addFromString(basename($file),$download_file);
-	}
-    $zip->close();
-header('Content-disposition: attachment; filename = "my file.zip"');
-header('Content-type: application/zip');
-header("Content-Length: " . filesize($yourfile));
+//Zip files
+$t = time();
+$zipname = date(DATE_ATOM,$t).".zip";
+$tmp_file = tempnam(sys_get_temp_dir(),"zip");
+
+$zip = new ZipArchive();
+$zip->open($tmp_file, ZIPARCHIVE::CREATE | ZIPARCHIVE::OVERWRITE);
+foreach ($fileArray as $file) {
+		$zip->addFile($file);
+}
+$zip->close();
+
+
+header('Content-Type: application/zip');
+header('Content-Length: ' . filesize($tmp_file));
+header('Content-Disposition: attachment; filename=\''.$zipname.'\'');
+ob_clean();
+flush();
 readfile($tmp_file);
-unlink($tmp_file);
+
+ignore_user_abort(true);
+if (connection_aborted()) {
+	unlink($tmp_file);
+	foreach($selectedArray as $selected_file){
+		unlink($selected_file);
+	}
+}
+
+
+
+/*-------------------------FUNCTIOINS--------------------------*/
+
+function get_download_file($url, $file_name, $file_size){
+	//echo $url."\n";
+	$output_filename = $file_name;
+	//echo $output_filename."\n";
+	
+	set_time_limit(0);
+	
+	//Get the file from remote server
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_VERBOSE, 1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+	//curl_setopt($ch, CURLOPT_REFERER, "https://dumps.wikimedia.org/scwiki/20181201/scwiki-20181201-pages-logging.xml.gz");
+	//curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 300);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); 
+	curl_setopt($ch, CURLOPT_NOPROGRESS,false);
+	
+	
+	//bytes to Mb
+	$memory_use_inMB = memory_get_usage()/1024/1024;
+
+	
+	$new_size = 128 + $file_size;
+
+	ini_set('memory_limit',strval($new_size).'M');
+	
+	$memory = ini_get('memory_limit');
+	//echo $memory."\n";
+	
+	$result = curl_exec($ch);
+	
+	if(!$result){
+		//echo 'error:' . curl_error($ch)."\n";
+	 }
+	curl_close($ch);
+
+	 //print_r($result); // prints the contents of the collected file before writing..
+	
+	file_put_contents($output_filename, $result); 
+	
+	ini_set('memory_limit', '128M');
+	
+	return($output_filename);
+}
+
 ?>
 </body>
 </html>
