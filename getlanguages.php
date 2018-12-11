41e65a5144461dadf9b98a056043792c34e69fcc
tongyewjun <tongyewjun@gmail.com>
2018-12-11 15:12:34 +0800

    added comment and cleaned up code

--- getlanguages.php
+++ getlanguages.php
@@ -35,14 +35,14 @@
 	
 
 /*Get Language & Code from meta wikimedia*/
-	//echo '<br/>';
 	
 	$langArray[] = [];
 	$codeArray[] = [];
 	$count2D = 0;
 	
+	//get all html source from the link and store in $query
 	$curl_handle=curl_init();
-    curl_setopt($curl_handle, CURLOPT_URL,'https://meta.wikimedia.org/wiki/Template:List_of_language_names_ordered_by_code');
+    curl_setopt($curl_handle, CURLOPT_URL,'https://meta.wikimedia.org/wiki/Template:List_of_language_names_ordered_by_code');		//get html source from url
     curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
     curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($curl_handle, CURLOPT_USERAGENT, 'Mozilla/5.0');
@@ -51,10 +51,12 @@
     $query = curl_exec($curl_handle);
     curl_close($curl_handle);
 	
+	//look for elements with tag <tr> in the html
 	$dom = new DOMDocument;
 	$dom->loadHTML($query);
 	$tags = $dom->getElementsByTagName('tr');
 	
+	//
 	function get_string_between($string, $start, $end){
 		$string = ' ' . $string;
 		$ini = strpos($string, $start);
@@ -64,40 +66,41 @@
 		return substr($string, $ini, $len);
 	}
 
+	//for each element in <tr> tag, get the language and code only
 	foreach ($tags as $tag) {
 		
+		//getting the content in the table 
 		if(preg_match("/^[a-z]/", $tag->nodeValue )){
 			
+			// split the phrase by any number of commas or space characters,
+			// which include " ", \r, \t, \n and \f
 			$code = preg_split("/[\s,]+/", $tag->nodeValue);
 			
+			//Find the position of the first occurrence of 'ltr' & 'rtl' and 
+			//get the string between code and 'ltr'/'rtl' and save as language
 			if(strpos($tag->nodeValue, 'ltr'))
 				$language = get_string_between($tag->nodeValue, $code[0], 'ltr');
 			else
 				$language = get_string_between($tag->nodeValue, $code[0], 'rtl');
 			
+			//replace the '-' with '_' to match the code get from https://dumps.wikimedia.org/backup-index.html
 			if(strpos($code[0], '-'))
 				$code[0] = str_replace("-","_",$code[0]);
 			
+			//store language and code into array 
 			$codeArray[$count2D] = $code[0];
-			
-			//echo $language."<br/>";
-			//echo($keywords[0]);
-			//echo '<br>';
-			//echo($keywords[1]);
-			//echo '<br>';
-			//echo '<br>';
-			
 			$langArray[$count2D] = array($code[0], $language);
-			//echo $langArray[$count2D][0]." ".$langArray[$count2D][1]."<br/>";
+			
 			$count2D++;
 		}
 	}
 	
 /*Find intersect Language*/
 
+	//find the code that matches from dump file with wiki available code
 	$results=array_intersect($codeArray,$langarray);
-	//print_r($results);
 	
+	//show the language in the dropdown based on available code
 	foreach($results as $result){
 		for($i=0; $i<$count2D; $i++){
 			if($result == $langArray[$i][0]){
